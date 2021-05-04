bd91b009011de3a929e10de4320dab1a44ef1d1e
briansimiyuj <briansimiyuj@gmail.com>
2021-05-04 13:40:43 +0300

    Accessing DOM

---
+++ script.js
@@ -0,0 +1,20 @@
+const musicContainer = document.querySelector('.music-container'),
+
+    playBtn = document.querySelector("#play"),
+
+    prevBtn = document.querySelector("#prev"),
+
+    nextBtn = document.querySelector("#next"),
+
+    audio = document.querySelector("audio"), 
+
+    progress  = document.querySelector(".progress"),
+
+    progressContainer = document.querySelector('.progress-container'),
+
+    title = document.querySelector("#title"),
+
+    cover = document.querySelector(".img-container img")
+
+
+console.log(musicContainer, playBtn, prevBtn, nextBtn, audio, progress, progressContainer, title, cover)     
